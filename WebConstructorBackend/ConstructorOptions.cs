9e9921e38a76fa179849aa0c1fe7e194f4b11e27
SemyonDuB <dubskikhsemyon@gmail.com>
2024-04-25 22:14:41 +0500

    Create template installer service

---
+++ WebConstructorBackend/ConstructorOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace WebConstructorBackend;
+
+public class ConstructorOptions
+{
+	public const string Name = "ConstructorOptions";
+	
+	public required Uri TemplateUrl { get; set; }
+	public required string SitesDirectory { get; set; } = "";
+	public string TemplateDirectory { get; set; } = "";
+}
---
+++ WebConstructorBackend/Services/TemplateInstallerService.cs
@@ -0,0 +1,34 @@
+﻿using System.IO.Compression;
+using System.Reflection;
+using Microsoft.Extensions.Options;
+
+namespace WebConstructorBackend.Services;
+
+public class TemplateInstallerService(
+	IConfiguration configuration,
+	IOptions<ConstructorOptions> options,
+	IHttpClientFactory clientFactory) : BackgroundService
+{
+	private ConstructorOptions Options { get; } = options.Value;
+	private IConfiguration Configuration { get; } = configuration;
+	private IHttpClientFactory HttpClientFactory { get; } = clientFactory;
+
+	protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+	{
+		var client = HttpClientFactory.CreateClient();
+		var stream = await client.GetStreamAsync(Options.TemplateUrl, stoppingToken);
+
+		var zipArchive = new ZipArchive(stream);
+
+		var templateDirectoryLocation = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location) ??
+		                                throw new NotImplementedException();
+		var zipDirectory = zipArchive.Entries.First().FullName;
+
+		await Task.Run(
+			() => zipArchive.ExtractToDirectory(templateDirectoryLocation, overwriteFiles: true),
+			stoppingToken);
+
+		Options.TemplateDirectory = Path.Combine(templateDirectoryLocation, zipDirectory);
+		Configuration.GetSection(ConstructorOptions.Name).Bind(Options);
+	}
+}
